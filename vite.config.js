5e7fa3f4df1bfb0638b256411b9c5b8c3efc88d0
BrightTetena <166717383+BrightTetena@users.noreply.github.com>
2025-06-03 19:02:25 +0200

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,22 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    hmr: true,
-    watch: {
-      usePolling: true,
-    },
-  },
-  optimizeDeps: {
-    force: true
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: `[name].[hash].js`,
-        chunkFileNames: `[name].[hash].js`,
-        assetFileNames: `[name].[hash].[ext]`,
-      },
-    },
-  },
 })
