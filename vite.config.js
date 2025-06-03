2a3559faa5e5d374c2b83d19f521a17eb53649c1
BrightTetena <166717383+BrightTetena@users.noreply.github.com>
2025-06-03 18:55:50 +0200

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -11,17 +9,15 @@ export default defineConfig({
       usePolling: true,
     },
   },
-  // Force Vite to always re-bundle and not use the cache
   optimizeDeps: {
     force: true
   },
-  // Add a timestamp to the file names in production to bust cache
   build: {
     rollupOptions: {
       output: {
-        entryFileNames: `[name].[hash].[time].js`,
-        chunkFileNames: `[name].[hash].[time].js`,
-        assetFileNames: `[name].[hash].[time].[ext]`,
+        entryFileNames: `[name].[hash].js`,
+        chunkFileNames: `[name].[hash].js`,
+        assetFileNames: `[name].[hash].[ext]`,
       },
     },
   },
